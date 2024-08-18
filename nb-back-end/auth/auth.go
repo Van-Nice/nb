3015922c81148225af2671efbdfd41f700841d11
Van-Nice <wilsonwalter808@gmail.com>
2024-08-18 13:13:30 -0600

    Refactor auth validating functions to separate file

--- nb-back-end/auth/auth.go
+++ nb-back-end/auth/auth.go
@@ -10,58 +10,15 @@ import (
 	"nb-back-end/emailer"
 	"net/http"
 	"os"
-	"regexp"
 	"strconv"
 	"time"
-	"unicode"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
-    "github.com/golang-jwt/jwt/v5"
 )
 
-func validateName(name string) (bool, bool) {
-    minLength := len(name) >= 2
-    isAlphabetic := true
-    for _, char := range name {
-        if !unicode.IsLetter(char) {
-            isAlphabetic = false
-            break
-        }
-    }
-    return minLength, isAlphabetic
-}
 
-func validateEmail(email string) bool {
-    return regexp.MustCompile(`^[^\s@]+@[^\s@]+\.[^\s@]+$`).MatchString(email)
-}
-
-func validatePassword(password string) (bool, bool, bool, bool, bool, bool) {
-    minLength := len(password) >= 8
-    hasUpperCase := regexp.MustCompile(`[A-Z]`).MatchString(password)
-    hasLowerCase := regexp.MustCompile(`[a-z]`).MatchString(password)
-    hasNumber := regexp.MustCompile(`\d`).MatchString(password)
-    hasSpecialChar := regexp.MustCompile(`[!@#$%^&*(),.?":{}|<>]`).MatchString(password)
-    noSpaces := regexp.MustCompile(`^[^\s]+$`).MatchString(password)
-    return minLength, hasUpperCase, hasLowerCase, hasNumber, hasSpecialChar, noSpaces
-}
-
-func validateDate(date string) (bool, bool, bool) {
-    regex := regexp.MustCompile(`^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$`)
-    formatValid := regex.MatchString(date)
-    year, month, day := 0, 0, 0
-    fmt.Sscanf(date, "%d-%d-%d", &year, &month, &day)
-    birthDate := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
-    today := time.Now()
-    age := today.Year() - birthDate.Year()
-    if today.YearDay() < birthDate.YearDay() {
-        age--
-    }
-    tooYoung := age < 13
-    tooOld := age > 120
-    return formatValid, tooYoung, tooOld
-}
 
 func hashPassword(password string) (string, error) {
     hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -106,7 +63,9 @@ func HandleCreateAccount(c *gin.Context) {
         c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid first name"})
         return
     }
-    if minLength, isAlphabetic := validateName(form.LastName); minLength && isAlphabetic {
+
+    minLength, isAlphabetic = validateName(form.LastName)
+    if !minLength || !isAlphabetic {
         c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid last name"})
         return
     }
